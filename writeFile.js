5db91bc69bc026362330e4abfdc76ac1745f60d1
vinicius Pasini <vinipasini@hotmail.com>
2023-08-25 13:06:45 +0200

    correcao

--- writeFile.js
+++ writeFile.js
@@ -4,7 +4,10 @@ import fs from 'fs'
 export async function writeFile(response){
     try {
         const path = process.env.TESTE == 'TRUE' ? './' : 'C:/CronJobs/SIE-API/'
-        const nameFile = response.fecha.replaceAll('/', '-')+'.txt'
+        let dateParts = response.fecha.split('/')
+        let dateObject = new Date(dateParts[2]+'-'+dateParts[1]+'-'+dateParts[0]);
+        dateObject.setDate(dateObject.getDate() + 1)
+        const nameFile = dateObject.getDate()+'-'+(dateObject.getMonth() + 1).toString().padStart(2, '0')+'-'+dateObject.getFullYear()+'-'+'.txt'
         await fs.promises.writeFile(path+nameFile,response.dato)
         console.log(chalk.green('fichero '+ nameFile +' creado'))
     } catch (err) {
