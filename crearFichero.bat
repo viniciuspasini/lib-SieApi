d2b6397c71c63c598836d0c89debde237d49cf63
vinicius Pasini <vinipasini@hotmail.com>
2023-08-28 09:00:14 +0200

    correccion

--- crearFichero.bat
+++ crearFichero.bat
@@ -1,2 +1,15 @@
-cd C:\Users\vpasini\Documents\lib-SieApi
+@echo off
+
+c:
+cd C:\Programs\lib-SieApi
+
+set Prog=crearFicheroSieApi
+call:LOG INICIO %Prog%
 npm run crear_fichero
+call:LOG FIN %Prog%
+
+goto:eof
+
+:LOG
+echo %date:~0,10%,%time:~0,8%,%~1 %~2 >> C:\CronJobs\crearFicheroSieApi.log
+goto:eof
--- writeFile.js
+++ writeFile.js
@@ -4,9 +4,10 @@ import fs from 'fs'
 export async function writeFile(response){
     try {
         const path = process.env.TESTE == 'TRUE' ? './' : 'C:/CronJobs/SIE-API/'
-        let dateParts = response.fecha.split('/')
-        let dateObject = new Date(dateParts[2]+'-'+dateParts[1]+'-'+dateParts[0]);
-        dateObject.setDate(dateObject.getDate() + 1)
+        //let dateParts = response.fecha.split('/')
+        //let dateObject = new Date(dateParts[2]+'-'+dateParts[1]+'-'+dateParts[0]);
+        let dateObject = new Date();
+        //dateObject.setDate(dateObject.getDate() + 1)
         const nameFile = dateObject.getDate()+'-'+(dateObject.getMonth() + 1).toString().padStart(2, '0')+'-'+dateObject.getFullYear()+'.txt'
         await fs.promises.writeFile(path+nameFile,response.dato)
         console.log(chalk.green('fichero '+ nameFile +' creado'))
